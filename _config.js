2459ba5b6bb3ebec699316dc59cd158422c37ce4
Meresia13 <melisaopiyo@gmail.com>
2024-04-13 17:13:07 +0300

    Updated _config.js file

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://melisaopiyo:ES3jIC2aOeuLEmol@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://melisaopiyo:ES3jIC2aOeuLEmol@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://melisaopiyo:ES3jIC2aOeuLEmol@java-todo.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://melisaopiyo:ES3jIC2aOeuLEmol@java-todo.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://melisaopiyo:ES3jIC2aOeuLEmol@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
