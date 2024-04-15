2776ff648b048bc86d2b45d689dd3a9621ee525e
Meresia13 <melisaopiyo@gmail.com>
2024-04-15 15:44:43 +0300

    Updated _config.js file

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://melisaopiyo:ES3jIC2aOeuLEmol@java-todo.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://melisaopiyo:ES3jIC2aOeuLEmol@java-todo.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://melisaopiyo:ES3jIC2aOeuLEmol@java-todo.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://melisaopiyo:ES3jIC2aOeuLEmol@moringaip1cluster.t7pmkv4.mongodb.net/?retryWrites=true&w=majority&appName=MoringaIP1Cluster',
+    development: 'mongodb+srv://melisaopiyo:ES3jIC2aOeuLEmol@moringaip1cluster.t7pmkv4.mongodb.net/?retryWrites=true&w=majority&appName=MoringaIP1Cluster',
+    test: 'mongodb+srv://melisaopiyo:ES3jIC2aOeuLEmol@moringaip1cluster.t7pmkv4.mongodb.net/?retryWrites=true&w=majority&appName=MoringaIP1Cluster',
 }
 module.exports = config;
